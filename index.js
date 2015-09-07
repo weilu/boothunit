3472d6c5364058309923d2455ebc0c882e8cef0c
Wei Lu <luwei.here@gmail.com>
2015-09-07 09:59:30 +0800

    Fix safari flakiness: read the image data only once
    also detects orientation from exif data

--- index.js
+++ index.js
@@ -3,42 +3,37 @@ var input = document.querySelector('input[type=file]')
 input.onchange = function () {
   var file = input.files[0]
 
-  draw(file, document.querySelector('#original'), 600, 400)
+  loadImage.parseMetaData(file, function (data) {
+    var options = {canvas: true, maxWidth: 600, minWidth: 600, cover: true}
 
+    if (data.exif) {
+      options.orientation = data.exif.get('Orientation')
+    }
+
+    loadImage(file, function(img){
+      var el = document.querySelector('#original')
+      el.width = img.width
+      el.height = img.height
+
+      var ctx = el.getContext('2d')
+      ctx.drawImage(img, 0, 0)
+      drawFiltered()
+    }, options)
+  })
+}
+
+function drawFiltered() {
   Array.prototype.forEach.call(document.querySelectorAll('.filter'), function(el) {
-    draw(file, el, 300, 200, function() {
-      Caman("#" + el.id, function () {
-        this[el.id]()
-        this.render()
-      })
+    var ctx = el.getContext('2d')
+    var sourceCanvas = document.querySelector('#original')
+    el.width = sourceCanvas.width / 2
+    el.height = sourceCanvas.height / 2
+
+    ctx.drawImage(sourceCanvas, 0, 0, el.width, el.height)
+
+    Caman("#" + el.id, function () {
+      this[el.id]().render()
     })
   })
 }
 
-function draw(file, el, width, height, callback) {
-  var reader = new FileReader();
-
-  reader.onload = function (e) {
-    var dataURL = e.target.result
-    var ctx = el.getContext('2d')
-    var img = new Image()
-
-    img.onload = function() {
-      if (img.width > img.height) {
-        el.width = width
-        el.height = height
-      } else {
-        el.width = height
-        el.height = width
-      }
-      ctx.clearRect(0, 0, el.width, el.height)
-      ctx.drawImage(img, 0, 0, el.width, el.height)
-    }
-
-    img.src = dataURL
-
-    if(callback) callback();
-  }
-
-  reader.readAsDataURL(file)
-}
