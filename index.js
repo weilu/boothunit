cd0ada948ee3e14df73d6ed351958d6599e13b08
Wei Lu <luwei.here@gmail.com>
2015-09-07 09:59:37 +0800

    clean up and add comments

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var input = document.querySelector('input[type=file]')
 var FILTER_HEIGHT = 300
 
+var input = document.querySelector('input[type=file]')
 var dataEl = document.createElement('canvas')
 var displayEl = document.querySelector('#original')
 
@@ -8,7 +8,11 @@ input.onchange = function () {
   var file = input.files[0]
 
   loadImage.parseMetaData(file, function (data) {
-    var options = {canvas: true, maxHeight: window.innerHeight - FILTER_HEIGHT - 100, cover: true}
+    var options = {
+      canvas: true,
+      cover: true,
+      maxHeight: window.innerHeight - FILTER_HEIGHT - 100
+    }
 
     if (data.exif) {
       options.orientation = data.exif.get('Orientation')
@@ -18,6 +22,7 @@ input.onchange = function () {
       var width = img.width
       var height = img.height
 
+      // shrink the image to fit viewport width
       if (img.width > window.innerWidth) {
         width = window.innerWidth
         height = window.innerWidth * img.height / img.width
@@ -32,7 +37,8 @@ input.onchange = function () {
       // back up the original data
       cloneCanvas(img, dataEl)
 
-      drawFiltered(displayEl)
+      drawFilters(displayEl)
+
     }, options)
   })
 }
@@ -44,7 +50,7 @@ window.addEventListener('load', function(){
     }, false)
 })
 
-function drawFiltered(sourceCanvas) {
+function drawFilters(sourceCanvas) {
   var width = FILTER_HEIGHT
   Array.prototype.forEach.call(document.querySelectorAll('.filters canvas'), function(el) {
     var ctx = el.getContext('2d')
@@ -64,7 +70,7 @@ function drawFiltered(sourceCanvas) {
     ctx.drawImage(sourceCanvas, x, y, sourceWidth, sourceWidth, 0, 0, width, width)
 
     Caman("#" + el.id, function () {
-      this.reloadCanvasData()
+      this.reloadCanvasData() // necessary if user replaces the current photo
       this[el.id]().render()
     })
   })
