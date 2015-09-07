79833028c4cb92d80da87293649772ae94952ea4
Wei Lu <luwei.here@gmail.com>
2015-09-07 09:59:24 +0800

    filters v1
    works on chrome but flakey on safari

--- index.js
+++ index.js
@@ -3,27 +3,42 @@ var input = document.querySelector('input[type=file]')
 input.onchange = function () {
   var file = input.files[0]
 
-  drawOnCanvas(file)
+  draw(file, document.querySelector('#original'), 600, 400)
+
+  Array.prototype.forEach.call(document.querySelectorAll('.filter'), function(el) {
+    draw(file, el, 300, 200, function() {
+      Caman("#" + el.id, function () {
+        this[el.id]()
+        this.render()
+      })
+    })
+  })
 }
 
-function drawOnCanvas(file) {
+function draw(file, el, width, height, callback) {
   var reader = new FileReader();
 
   reader.onload = function (e) {
     var dataURL = e.target.result
-    var c = document.querySelector('canvas')
-    var ctx = c.getContext('2d')
+    var ctx = el.getContext('2d')
     var img = new Image()
 
     img.onload = function() {
-      c.width = img.width
-      c.height = img.height
-      ctx.drawImage(img, 0, 0)
+      if (img.width > img.height) {
+        el.width = width
+        el.height = height
+      } else {
+        el.width = height
+        el.height = width
+      }
+      ctx.clearRect(0, 0, el.width, el.height)
+      ctx.drawImage(img, 0, 0, el.width, el.height)
     }
 
     img.src = dataURL
+
+    if(callback) callback();
   }
 
   reader.readAsDataURL(file)
 }
-
