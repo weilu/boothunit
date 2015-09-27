f60fb9ed901418acf26d867577623f51f458fbb7
Wei Lu <luwei.here@gmail.com>
2015-09-27 11:05:51 +0800

    attempt to set up flash messages WIP

--- server.js
+++ server.js
@@ -6,15 +6,22 @@ var mkdirp = require('mkdirp')
 var express = require('express')
 var multer  = require('multer')
 var upload = multer({ storage: multer.memoryStorage() })
+var session = require('express-session')
+var cookieParser = require('cookie-parser')
+var flash = require('connect-flash')
 
 var app = express()
 
+app.use(cookieParser(process.env.COOKIE_SECRET))
+app.use(session({cookie: {maxAge: 60000}}))
+app.use(flash())
+app.set('view engine', 'jade')
+app.set('views', './templates')
+
+app.use('/', express.static(path.join(__dirname, 'app')))
 app.get('/*', function(req, res, next) {
-  if (path.extname(req.url) == '') {
-    req.originalUrl = '/'
-    req.url = '/'
-  }
-  express.static(path.join(__dirname, 'app'))(req, res, next)
+  console.log(req.flash('info'))
+  res.render('index', { messages: req.flash('info') })
 })
 
 app.post('/*', upload.single('photo'), function (req, res) {
@@ -24,8 +31,10 @@ app.post('/*', upload.single('photo'), function (req, res) {
     console.log(filename)
     fs.writeFile(filename, req.file.buffer, function (err) {
       if (err) {
+        flash('error', 'Failed to upload. Try again.')
         console.log(err)
       }
+      flash('info', 'Sent to print queue. Go pick it up!')
       res.redirect("back")
     })
   })
